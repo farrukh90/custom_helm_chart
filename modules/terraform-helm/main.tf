8c58bdd30b2ff3a56fecbec3222ff364a83241d9
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-09-24 10:30:22 -0400

    sdf

--- modules/terraform-helm/main.tf
+++ modules/terraform-helm/main.tf
@@ -1,3 +1,8 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
 resource "helm_release" "helm_deployment" {
   name      = var.deployment_name
   namespace = var.deployment_namespace
--- modules/terraform-k8s-namespace/main.tf
+++ modules/terraform-k8s-namespace/main.tf
@@ -1,3 +1,8 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
 resource "kubernetes_namespace" "k8s-namespace" {
   metadata {
     name = var.deployment_namespace
