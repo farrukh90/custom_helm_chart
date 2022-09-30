8e1d7f96fd594685e4c20a98280a41160853b2d9
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-09-30 12:16:20 -0400

    force added

--- modules/terraform-helm/main.tf
+++ modules/terraform-helm/main.tf
@@ -2,6 +2,7 @@ resource "helm_release" "helm_deployment" {
   name      = var.deployment_name
   namespace = var.deployment_namespace
   chart     = var.deployment_path
+  force_update = true
   wait = false
 
   values = [
