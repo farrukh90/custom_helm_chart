80ec66e209c6ab380a42f79735936c3177e173b1
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-11-25 17:46:00 -0600

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -2,7 +2,7 @@ DIR=$(pwd)
 cat << EOF > "$DIR/backend.tf"
 terraform {
   backend "gcs" {
-    bucket  = "`gcloud alpha storage ls| grep "backend-" | awk -F "/" '{print $3}'`"
+    bucket  = "`gcloud alpha storage ls| grep "terraform-project-" | awk -F "/" '{print $3}'`"
     prefix  = "/dev`pwd`"
   }
 }
